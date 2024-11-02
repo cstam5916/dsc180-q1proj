4f1760523ea54e88428cfac1f8d0e4da99655545
calebstamucsd <cstam@ucsd.edu>
2024-11-02 13:01:48 -0700

    GAT and graph classification on ENZYMES

--- gcn.py
+++ gcn.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, GINConv
+from torch_geometric.nn import GCNConv
 from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool
 
 class GCN(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
@@ -16,3 +17,24 @@ class GCN(torch.nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
         return F.log_softmax(x, dim=1)
+    
+class GCNGraphLev(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.linear = Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.3, training=self.training)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.3, training=self.training)
+        x = self.conv3(x, edge_index)
+
+        x = global_mean_pool(x, batch)
+        x = self.linear(x)
+        return F.log_softmax(x, dim=-1)
--- train_cora.py
+++ train_cora.py
@@ -2,7 +2,7 @@ import torch
 import torch_geometric as pyg
 from gcn import GCN
 from gin import GIN
-# from gat import GAT
+from gat import GAT
 from tqdm.auto import tqdm
 import torch.nn.functional as F
 
@@ -14,23 +14,45 @@ def main():
     d_in, d_lat, d_out = dataset.num_node_features, 64, dataset.num_classes
     gcn = GCN(d_in, d_lat, d_out).to(device)
     gin = GIN(d_in, d_lat, d_out).to(device)
-    # gat = GAT(d_in, d_lat, d_out)
-    acc_list = []
-    for model in [gcn, gin, ]: # add gat
+    gat = GAT(d_in, d_lat, d_out, heads=16).to(device)
+    train_acc_list = []
+    test_acc_list = []
+    for model in [gcn, gin, gat]: # add gat
         optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+        val_loss_list = []
+        val_model_params = []
         model.train()
         for epoch in tqdm(range(300)):
+            model.train()
             optimizer.zero_grad()
             out = model(dataset)
             loss = F.nll_loss(out[dataset.train_mask], dataset.y[dataset.train_mask])
             loss.backward()
             optimizer.step()
+
+            if(epoch % 10 == 0):
+                model.eval()
+                out = model(dataset)
+                val_loss = F.nll_loss(out[dataset.val_mask], dataset.y[dataset.val_mask]).item()
+                if((len(val_loss_list) >= 5) and all(x < val_loss for x in val_loss_list[-5:])):
+                    best_model_state = val_model_params[val_loss_list.index(min(val_loss_list))]
+                    model.load_state_dict(best_model_state)
+                    print(f'Early Stopping at Epoch {epoch}')
+                    break
+                else:
+                    val_loss_list.append(val_loss)
+                    val_model_params.append(model.state_dict())
         model.eval()
         pred = model(dataset).argmax(dim=1)
-        correct = (pred[dataset.test_mask] == dataset.y[dataset.test_mask]).sum()
-        acc = int(correct) / int(dataset.test_mask.sum())
-        acc_list.append(acc)
-    print(acc_list)
+        train_correct = (pred[dataset.train_mask] == dataset.y[dataset.train_mask]).sum()
+        train_acc = int(train_correct) / int(dataset.train_mask.sum())
+        train_acc_list.append(train_acc)
+
+        test_correct = (pred[dataset.test_mask] == dataset.y[dataset.test_mask]).sum()
+        test_acc = int(test_correct) / int(dataset.test_mask.sum())
+        test_acc_list.append(test_acc)
+
+    print(train_acc_list, test_acc_list)
 
 
 if(__name__ == '__main__'):
