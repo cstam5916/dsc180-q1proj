cc34e552cb09c8b35db3321092a9095f88e591a3
calebstamucsd <cstam@ucsd.edu>
2024-11-02 13:31:20 -0700

    fixing ENZYMES train loop

--- train_enzymes.py
+++ train_enzymes.py
@@ -7,7 +7,6 @@ from tqdm.auto import tqdm
 import torch.nn.functional as F
 from torch_geometric.loader import DataLoader
 import random
-random.seed(21)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using device {device}')
@@ -15,17 +14,18 @@ print(f'Using device {device}')
 def train(model, dataloader, criterion, optimizer):
     model.train()
     for data in dataloader:  # Iterate in batches over the training dataset.
+         data = data.to(device)
          out = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
          loss = criterion(out, data.y)  # Compute the loss.
          loss.backward()  # Derive gradients.
          optimizer.step()  # Update parameters based on gradients.
          optimizer.zero_grad()  # Clear gradients.
-         return loss
 
 def get_acc(model, dataloader, criterion):
      model.eval()
      correct = 0
      for data in dataloader:  # Iterate in batches over the training/test dataset.
+         data = data.to(device)
          out = model(data.x, data.edge_index, data.batch)  
          loss = criterion(out, data.y) 
          pred = out.argmax(dim=1)  # Use the class with highest probability.
@@ -33,12 +33,12 @@ def get_acc(model, dataloader, criterion):
      return loss, (correct / len(dataloader.dataset))  # Derive ratio of correct predictions.
 
 def main():
-    dataset = pyg.datasets.TUDataset(root='data/TUDataset', name='ENZYMES', force_reload=True).to(device)
+    dataset = pyg.datasets.TUDataset(root='data/TUDataset', name='ENZYMES', force_reload=True)
 
     dataset = dataset.shuffle()
-    train_dataset = dataset[:500]
-    val_dataset = dataset[500:550]
-    test_dataset = dataset[550:600]
+    train_dataset = dataset[:420]
+    val_dataset = dataset[420:510]
+    test_dataset = dataset[510:]
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
@@ -62,9 +62,9 @@ def main():
             train(model, train_loader, criterion, optimizer)
             val_loss, _ = get_acc(model, val_loader, criterion)
 
-            if((len(val_loss_list) >= 30) and val_loss > val_loss_list[-1]):
+            if((len(val_loss_list) >= 30) and (val_loss > min(val_loss_list))):
                 patience_counter += 1
-                if(patience_counter > 10):
+                if(patience_counter > 15):
                     best_model_state = val_model_params[val_loss_list.index(min(val_loss_list))]
                     model.load_state_dict(best_model_state)
                     print(f'Early Stopping at Epoch {epoch}')
